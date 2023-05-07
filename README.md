4ba378f23a8590be6c498e39db9d07c4b521361b
DongHo Lim <mashidavid2@gmail.com>
2023-05-07 21:45:11 +0900

    update readme

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
-## Personalized Item Recommendation model
+## AutoFIS Recommendation model with MovieLen 1M dataset
 
-### Model Test
 #### requirements
-- venv 구성하여 requirements.txt에 있는 dependency python package 추가 필요
+- I recommend you to create virtual environment for running the code 
 ```
-$ python -m venv $(venv_name)
-$ source $(venv_path)/bin/activate
+$ conda create -n $(the name of your env) python=3.10
 $ pip install -r $(requirements.txt path)
 ```
+some errors could come up while installing the requirements. If so, you should have to install them one by one.
 
-#### sample data prepare
-- ml-1m public dataset을 이용하여 test
-- ml-1m public dataset (users.dat, movies.dat, ratings.dat)을 test를 위한 csv data format으로 변환
-- src/tmp/dataset_ml-1m 에 해당 public dataset을 넣어 주어야 함
-- dataset 및 학습 결과 들은 tmp 내에서 관리하여 gitignore처리 
+#### sample raw data preprocessing
+- for my code, I used ml-1m benchmark dataset
+- sample_data_prepare.py converts ml-1m public dataset (users.dat, movies.dat, ratings.dat) into csv data format
+- you should put the benchmark dataset at src/tmp/dataset_ml-1m
 
-#### model test
-- args로 train 돌릴 model 및 dataset 정보를 넣어서 test
-- model hyperparameter를 입력하지 않을 경우, 각 ModelInfo dataclass에서 정의한 default 값 사용
-- 예시
+- To use other datasets, you have to modify the preprocess part of the code by yourselves as of right now. Could be updated later on.
+
+#### model train
+- I run the model with the information in the args or configurations
+- If you don't insert model hyperparameters, it will use default values in the ModelInfo dataclass
+- Example
 ```
-## model_test.py 
+## train.py 
 
 column_info = {
         'rating_column': {'name': 'Rating', 'type': 'Numerical'},
--- src/train.py
+++ src/train.py
@@ -4,15 +4,11 @@ import argparse
 import json
 import yaml
 import os.path as path
-#module_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 module_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
-# print(module_path)
 if module_path not in sys.path:
     sys.path.append(module_path)
-#sys.path.append(os.path.abspath(__file__))
-from classes import ColumnInfo, ModelInfoMapper, RecommendationInfo
+from classes import ColumnInfo, ModelInfoMapper
 from executor import ExecutorFactory
-from preprocess import TableReader
 from utils import from_dict, yaml_to_model_dict, yaml_to_config_dict
 
 package_path = path.dirname(path.dirname(path.abspath(__file__)))
@@ -43,11 +39,12 @@ def train():
         'timestamp_column': {'name': '', 'type': 'TimeStamp'}
     }
     current_path = path.dirname(path.abspath(__file__))
+    dataset = 'ml-1m'
     save_dir = path.join(current_path, 'save')
-    train_dir = path.join(current_path, 'tmp/dataset_ml-1m')
-    interaction_path = path.join(train_dir, 'ml_1m_interaction_table.csv')
-    user_path = path.join(train_dir, 'ml_1m_user_table.csv')
-    item_path = path.join(train_dir, 'ml_1m_item_table.csv')
+    train_dir = path.join(current_path, 'tmp/dataset_'+ dataset)
+    interaction_path = path.join(train_dir, dataset +'_interaction_table.csv')
+    user_path = path.join(train_dir, dataset +'_user_table.csv')
+    item_path = path.join(train_dir, dataset +'_item_table.csv')
     model_info_mapper = ModelInfoMapper(model_name, model_info_dict)
 
     executor = ExecutorFactory.from_model_name(model_name)
@@ -66,7 +63,7 @@ def parse_train_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--column_info', type=str, default='')
     parser.add_argument('--recommendation_info', type=str, default='')
-    parser.add_argument('--models_info', type=str, default='') # 기존 version front 호환을 위한 임시 argument
+    parser.add_argument('--models_info', type=str, default='') 
     parser.add_argument('--top_k', type=int, default=10)
     args = parser.parse_args()
     return args
@@ -74,52 +71,4 @@ def parse_train_args():
 
 if __name__ == '__main__':
     args = parse_train_args()
-    # # column_info = {
-    # #     'col_rating_name': {'name': 'Rating', 'type': 'num'},
-    # #     'col_user_name': {'name': 'UserID', 'type': 'cat'},
-    # #     'col_user_features': [
-    # #         {'name': 'Gender', 'type': 'cat'},
-    # #         {'name': 'Age', 'type': 'num'},
-    # #         {'name': 'Occupation', 'type': 'cat'}
-    # #     ],
-    # #     'col_item_name': {'name': 'MovieID', 'type': 'cat'},
-    # #     'col_item_features': [
-    # #         # {'name': 'Genre', 'type': 'cat'},
-    # #         {'name': 'Year', 'type': 'num'}
-    # #     ],
-    # #     'col_temporal_name': {'name': '', 'type': 'tem'}
-    # # }
-    # recommendation_info_cat_boost = {
-    #     'recommendation_type': 'USER2ITEM',
-    #     'model_name': 'CatBoost',
-    #     'models_info': {
-    #         'CatBoost': {
-    #             'model_name': 'CatBoost',
-    #             'epoch': 1000,
-    #             'batch_size': 2048,
-    #             'num_negative': 4,
-    #             'learning_rate': 0.01,
-    #             'depth': 10
-    #             # 'depth': 1
-    #         },
-    #     }
-    # }
-    #
-    # recommendation_info_autofis = {
-    #     'recommendation_type': 'USER2ITEM',
-    #     'model_name': 'AutoFis',
-    #     'models_info': {
-    #         'AutoFis': {
-    #             'model_name': 'AutoFis',
-    #             'batch_size': 2048,
-    #             'num_negative': 4,
-    #             'learning_rate': 0.001,
-    #             'latent_dim': 10,
-    #             'epoch': 100,
-    #         },
-    #     }
-    # }
-    # # args.column_info = json.dumps(column_info)
-    # args.recommendation_info = json.dumps(recommendation_info_autofis)
-    # # args.recommendation_info = json.dumps(recommendation_info_cat_boost)
     train()
